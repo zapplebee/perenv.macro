015ed365a76008be39b2cb412fc33d15b37bea75
Zachary Skalko <zac_skalko@cargill.com>
2020-09-11 23:05:26 -0500

    Overload signature with object

--- perenv.macro.js
+++ perenv.macro.js
@@ -2,10 +2,49 @@ const { createMacro } = require("babel-plugin-macros");
 const path = require("path");
 
 function loadPerEnvMacro({ state, babel, references }) {
+  const program = state.file.path;
   const { loadPerEnv = [] } = references;
 
   loadPerEnv.forEach((reference) => {
-    const importableFilePath = reference.parentPath.node.arguments[0].value;
+    const configObject = {
+      path: null,
+      identifier: null,
+    };
+
+    if (reference.parentPath.node.arguments[0].type === "StringLiteral") {
+      configObject.path = reference.parentPath.node.arguments[0].value;
+    }
+
+    if (reference.parentPath.node.arguments[0].type === "ObjectExpression") {
+      const arg = reference.parentPath.node.arguments[0];
+      const pathProperty = arg.properties.find((p) => p.key.name === "path");
+      configObject.path = pathProperty.value.value;
+      const identifierProperty = arg.properties.find(
+        (p) => p.key.name === "identifier"
+      );
+
+      const imports = program.node.body
+        .map((n) => n.specifiers)
+        .filter(Boolean)
+        .flat(1)
+        .map((i) => i.local.name);
+
+      const declarations = program.node.body
+        .map((n) => n.declarations)
+        .filter(Boolean)
+        .flat(1)
+        .map((d) => d.id.name);
+
+      if (
+        [...imports, ...declarations].includes(identifierProperty.value.value)
+      ) {
+        throw new Error(
+          `It seems you're trying to use an identifier that already exists with perenv.macro`
+        );
+      }
+      configObject.identifier = identifierProperty.value.value;
+    }
+
     const envKey = reference.parentPath.node.arguments[1].value;
 
     const doImport = reference.parentPath.node.arguments[2]
@@ -15,15 +54,19 @@ function loadPerEnvMacro({ state, babel, references }) {
     if (doImport) {
       const callerFilePath = state.file.opts.filename;
       const callerBaseDir = path.dirname(callerFilePath);
-      const importFilePath = path.relative(callerBaseDir, importableFilePath);
+      const importFilePath =
+        "./" + path.relative(callerBaseDir, configObject.path);
       const t = babel.types;
-      const id = reference.parentPath.scope.generateUidIdentifier("ENV_LOAD");
-      const program = state.file.path;
+
+      const importIdentifiers = [];
+
+      if (configObject.identifier) {
+        importIdentifiers.push(
+          t.importNamespaceSpecifier(t.identifier(configObject.identifier))
+        );
+      }
       program.node.body.unshift(
-        t.importDeclaration(
-          [t.importNamespaceSpecifier(id)],
-          t.stringLiteral(importFilePath)
-        )
+        t.importDeclaration(importIdentifiers, t.stringLiteral(importFilePath))
       );
     }
     reference.parentPath.remove();
--- perenv.spec.js
+++ perenv.spec.js
@@ -9,24 +9,84 @@ function applyMacro(src) {
   }).code.trim();
 }
 
-describe("Name of the group", () => {
+describe("String signature", () => {
   it("should import the first argument if the env specified is defined at all", () => {
     process.env.ENABLE_A_THING = "0";
     const result = applyMacro(`
-    import { loadPerEnv } from "./perEnv.macro";
-    loadPerEnv('./src/aaa.js', 'ENABLE_A_THING');
+      import { loadPerEnv } from "./perEnv.macro";
+      loadPerEnv('./aaa.js', 'ENABLE_A_THING');
+    `);
 
-`);
-
-    expect(result).toBe(`import * as _ENV_LOAD from "src/aaa.js";`);
+    expect(result).toBe(`import "./aaa.js";`);
   });
 
-  it("should not import the first argument if third argument does is not equal to the environmental variable", () => {
+  it("should import the first argument if third argument is equal to the environmental variable", () => {
     process.env.ENABLE_ANOTHER_THING = "1";
     const result = applyMacro(`
         import { loadPerEnv } from "./perEnv.macro";
-  loadPerEnv('./src/aaa.js', 'ENABLE_ANOTHER_THING', '2');`);
+        loadPerEnv('./aaa.js', 'ENABLE_ANOTHER_THING', '1');
+    `);
+
+    expect(result).toBe(`import "./aaa.js";`);
+  });
+
+  it("should not import the first argument if third argument does is not equal to the environmental variable", () => {
+    process.env.ENABLE_ONE_MORE_THING = "1";
+    const result = applyMacro(`
+        import { loadPerEnv } from "./perEnv.macro";
+        loadPerEnv('./src/aaa.js', 'ENABLE_ONE_MORE_THING ', '2');
+    `);
 
     expect(result).toBe(``);
+  });
+});
+
+describe("Object signature", () => {
+  it("should declare an idenfier if declared in the object signature overload", () => {
+    process.env.ENABLE_A_THING = "0";
+    const result = applyMacro(`
+      import { loadPerEnv } from "./perEnv.macro";
+      loadPerEnv({path: './ccc.js', identifier: 'thing'}, 'ENABLE_A_THING');
+    `);
+    expect(result).toBe(`import * as thing from "./ccc.js";`);
+  });
+
+  it("should throw an error if a duplicate identifier is an already defined variable", () => {
+    process.env.ENABLE_A_THING = "0";
+    expect(() =>
+      applyMacro(`
+      import { loadPerEnv } from "./perEnv.macro";
+      const alreadyDeclared = true
+      loadPerEnv({path: 'ccc.js', identifier: 'alreadyDeclared'}, 'ENABLE_A_THING');
+    `)
+    ).toThrow(
+      `It seems you're trying to use an identifier that already exists with perenv.macro`
+    );
+  });
+
+  it("should throw an error if a duplicate identifier is an already defined import", () => {
+    process.env.ENABLE_A_THING = "0";
+    expect(() =>
+      applyMacro(`
+      import { loadPerEnv } from "./perEnv.macro";
+      import alreadyDeclared from 'already.js'
+      loadPerEnv({path: 'ccc.js', identifier: 'alreadyDeclared'}, 'ENABLE_A_THING');
+    `)
+    ).toThrow(
+      `It seems you're trying to use an identifier that already exists with perenv.macro`
+    );
+  });
+
+  it("should throw an error if a duplicate identifier is an already destructured import", () => {
+    process.env.ENABLE_A_THING = "0";
+    expect(() =>
+      applyMacro(`
+      import { loadPerEnv } from "./perEnv.macro";
+      import { alreadyDeclared } from 'already.js'
+      loadPerEnv({path: 'ccc.js', identifier: 'alreadyDeclared'}, 'ENABLE_A_THING');
+    `)
+    ).toThrow(
+      `It seems you're trying to use an identifier that already exists with perenv.macro`
+    );
   });
 });
